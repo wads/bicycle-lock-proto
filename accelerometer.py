fbae8f580ca9b70b841adf9eeb71f1619cd06f29
Yoshitaka Wada <wadslab@gmail.com>
2017-08-12 04:40:22 +0900

    Mod spi receive data correctly

--- accelerometer.py
+++ accelerometer.py
@@ -5,7 +5,7 @@ spi.open(0, 0)
 
 def analog_read(channel):
     r = spi.xfer2([0x06, channel << 6, 0])
-    adc_out = ((r[1]&3) << 8) + r[2]
+    adc_out = ((r[1] & 0x0f) << 8) | r[2]
     return adc_out
 
 

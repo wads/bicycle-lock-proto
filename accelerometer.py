0df56a2ad79e2fed76a7ded37977648812f808cb
Yoshitaka Wada <wadslab@gmail.com>
2017-08-26 17:29:05 +0900

    Implement proto 1

--- accelerometer.py
+++ accelerometer.py
@@ -1,20 +1,55 @@
-import spidev, time
+import spidev
+import time
 
-spi = spidev.SpiDev()
-spi.open(0, 0)
+POWER_SUPPLY_VOLTAGE = 4.00  # [V]
+AMPLITUDE_PER_G = POWER_SUPPLY_VOLTAGE / 5 * 1000  # [mV]
+OFFSET_V = POWER_SUPPLY_VOLTAGE / 2 * 1000  # [mV]
+MV_PER_DEG = AMPLITUDE_PER_G / 90
 
-def analog_read(channel):
+CH_X = 0
+CH_Y = 1
+CH_Z = 2
+
+
+def offset_avg(spi, roop_cnt=100):
+    avg = [0, 0, 0]  # x, y, z
+    for i in range(0, roop_cnt):
+        avg[0] += analog_read(spi, CH_X)
+        avg[1] += analog_read(spi, CH_Y)
+        avg[2] += analog_read(spi, CH_Z)
+    return [i/roop_cnt for i in avg]
+
+
+def analog_read(spi, channel):
     r = spi.xfer2([0x06, channel << 6, 0])
     adc_out = ((r[1] & 0x0f) << 8) | r[2]
     return adc_out
 
 
-try:
-    while True:
-        x = analog_read(0)
-        y = analog_read(1)
-        z = analog_read(2)
-        print ("x=%d, y=%d, z=%d" % (x, y, z))
-        time.sleep(1)
-except:
-    print "end\n"
+def analog_read_x(spi):
+    return analog_read(spi, CH_X)
+
+
+def analog_read_y(spi):
+    return analog_read(spi, CH_Y)
+
+
+def analog_read_x(spi):
+    return analog_read(spi, CH_Z)
+
+
+def degree(spi, channel, offset):
+    value = analog_read(spi, channel)
+    return (value - offset) / MV_PER_DEG
+
+
+def degree_x(spi):
+    return degree(spi, CH_X, offset=OFFSET_V)
+
+
+def degree_y(spi):
+    return degree(spi, CH_Y, offset=OFFSET_V)
+
+
+def degree_z(spi):
+    return degree(spi, CH_X, offset=OFFSET_V)
--- buzzer.py
+++ buzzer.py
@@ -1,28 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
-buzzer_pin = 18
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(buzzer_pin, GPIO.OUT)
 
-def buzz(pitch, duration):
+def buzz(pin, pitch, duration):
     period = 1.0 / pitch
     delay = period / 2
     cycles = int(pitch * duration)
     for i in range(cycles):
-        GPIO.output(buzzer_pin, True)
+        GPIO.output(pin, True)
         time.sleep(delay)
-        GPIO.output(buzzer_pin, False)
+        GPIO.output(pin, False)
         time.sleep(delay)
 
 
-try:
-    GPIO.output(buzzer_pin, False) # init buzzer
-    while True:
-        pitch_s = raw_input("Enter Pitch (200 to 2000): ")
-        pitch = float(pitch_s)
-        duration_s = raw_input("Enter Duration (seconds): ")
-        duration = float(duration_s)
-        buzz(pitch, duration)
-except:
-    GPIO.cleanup()
+def stop(pin):
+    GPIO.output(pin, False)

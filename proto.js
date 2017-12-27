b5a1c7508093b980b22ab43efebf2441ac7d7e7f
Yoshitaka Wada <wadslab@gmail.com>
2017-12-27 11:30:38 +0900

    mod proto.js

--- proto.js
+++ proto.js
@@ -1,5 +1,44 @@
+"use strict";
+
+const Gpio = require('onoff').Gpio;
 const bleno = require('bleno');
 const util = require('util');
+
+// stepper motor
+//const coil_A_1_pin = new Gpio(14, 'out');
+//const coil_A_2_pin = new Gpio(15, 'out');
+//const coil_B_1_pin = new Gpio(17, 'out');
+//const coil_B_2_pin = new Gpio(18, 'out');
+
+const forward_seq = [[1, 0, 1, 0], [0, 1, 1, 0], [0, 1, 0, 1], [1, 0, 0, 1]];
+
+const stepMotor = function(steps, sequence, delay) {
+    console.log('-- stepMotor --');
+    if(steps === 0) {
+        return true;
+    }
+    for(let seq in sequence) {
+        console.log(sequence[seq][0]);
+        console.log(sequence[seq][1]);
+        console.log(sequence[seq][2]);
+        console.log(sequence[seq][3]);
+        coil_A_1_pin.writeSync(sequence[seq][0]);
+        coil_A_2_pin.writeSync(sequence[seq][1]);
+        coil_B_1_pin.writeSync(sequence[seq][2]);
+        coil_B_2_pin.writeSync(sequence[seq][3]);
+    }
+    setTimeout(stepMotor, delay, --steps, sequence, delay);
+};
+
+const forwardStepMotor = function(steps, delay) {
+    console.log('-- forwardStepMotor --');
+    setTimeout(stepMotor, delay, steps, forward_seq, delay);
+};
+
+const backwardStepMotor = function(steps, delay) {
+    console.log('-- backwardStepMotor --');
+    setTimeout(stepMotor, delay, steps, forward_seq.reverse(), delay);
+};
 
 // Service name
 // maximun 26 bytes
@@ -28,14 +67,24 @@ LockCharc.prototype.onReadRequest = function(offset, callback) {
 LockCharc.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
     console.log('onWriteRequest');
 
-    const instruction = data.toString('utf8').split(':')
-    const direction = instruction[0]
-    const count = instruction[1]
-    const cmd = 'python stepper.py -d ' + direction + ' -s ' + (count * 2)
-    console.log(cmd)
+    const instruction = data.toString('utf8').split(':');
+    const direction = instruction[0];
+    const steps = instruction[1];
 
-    const execSync = require('child_process').execSync;
-    const result = execSync(cmd);
+    //const cmd = 'python stepper.py -d ' + direction + ' -s ' + (steps * 2);
+    //console.log(cmd);
+    //const execSync = require('child_process').execSync;
+    //const result = execSync(cmd);
+
+    coil_A_1_pin.writeSync(0);
+    coil_A_2_pin.writeSync(0);
+    coil_B_1_pin.writeSync(0);
+    coil_B_2_pin.writeSync(0);
+    if(direction === 'l') {
+        forwardStepMotor(steps * 2, 10);
+    } else {
+        backwardStepMotor(steps * 2, 10);
+    }
 
     callback(this.RESULT_SUCCESS);
 };
@@ -62,4 +111,12 @@ bleno.on('advertisingStart', function(err) {
     if(!err) {
         bleno.setServices([moveService]);
     }
+});
+
+
+process.on('SIGINT', function () {
+    coil_A_1_pin.unexport();
+    coil_A_2_pin.unexport();
+    coil_B_1_pin.unexport();
+    coil_B_2_pin.unexport();
 });

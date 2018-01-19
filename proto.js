2b163d2b3f36c9320a0c2fa6906004c511fbf78c
Yoshitaka Wada <wadslab@gmail.com>
2018-01-20 02:43:43 +0900

    refactor proto.js

--- proto.js
+++ proto.js
@@ -8,11 +8,6 @@ const spi = require('spi-device');
 //
 // photoreflector
 //
-const COLLECTOR_BUFFER_SIZE = 100;
-const CHANNEL_SIZE = 4;
-const COLLECT_INTERVAL_MSEC = 100;
-const collector_buffer = new Array(CHANNEL_SIZE);
-const interval_ids = new Array(CHANNEL_SIZE);
 const tx_data_list = [
     new Buffer([0x06, 0x00, 0x00]), // ch1
     new Buffer([0x06, 0x40, 0x00]), // ch2
@@ -32,44 +27,6 @@ const extractValue = function(message) {
     return ((message.receiveBuffer[1] & 0x0f) << 8) + message.receiveBuffer[2];
 };
 
-const startCollectValue = function(channels) {
-    if(!Array.isArray(channels)) {
-        channels = [channels];
-    }
-
-    for(let ch of channels) {
-        if(!Number.isInteger(ch) || ch < 0 || ch > CHANNEL_SIZE) continue;
-        resetCollectValue(ch);
-        interval_ids[ch] = setInterval(collectValue, COLLECT_INTERVAL_MSEC, ch);
-    }
-};
-
-const resetCollectValue = function(ch) {
-    if(interval_ids[ch]) {
-        clearInterval(interval_ids[ch]);
-    }
-    collector_buffer[ch] = [];
-};
-
-const collectValue = function(channel) {
-    mcp3204.transfer([txData(channel)], function (err, msg) {
-        if (err) throw err;
-
-        if(collector_buffer[channel].length >= COLLECTOR_BUFFER_SIZE) {
-            collector_buffer[channel].pop();
-        }
-        collector_buffer[channel].unshift(extractValue(msg[0]));
-    });
-};
-
-const readyToRead = function() {
-    return collector_buffer[0].length < 30 || collector_buffer[1].length < 30;
-}
-
-const mcp3204 = spi.open(0, 0, function (err) {
-    if (err) throw err;
-    startCollectValue([0, 1]);
-});
 
 //
 // stepper motor
@@ -104,11 +61,107 @@ const backwardStep = function(steps, delay) {
 };
 
 //
+// corrector
+//
+const COLLECTOR_BUFFER_SIZE = 100;
+const CHANNEL_SIZE = 4;
+const COLLECT_INTERVAL_MSEC = 100;
+const collector_buffer = new Array(CHANNEL_SIZE);
+const interval_ids = new Array(CHANNEL_SIZE);
+
+const startCollectValue = function(channels) {
+    if(!Array.isArray(channels)) {
+        channels = [channels];
+    }
+
+    for(let ch of channels) {
+        if(!Number.isInteger(ch) || ch < 0 || ch > CHANNEL_SIZE) continue;
+        resetCollectValue(ch);
+        interval_ids[ch] = setInterval(collectValue, COLLECT_INTERVAL_MSEC, ch);
+    }
+};
+
+const stopCollectValue = function(channels) {
+    if(!Array.isArray(channels)) {
+        channels = [channels];
+    }
+
+    for(let ch of channels) {
+        resetCollectValue(ch);
+    }
+}
+
+const resetCollectValue = function(ch) {
+    if(interval_ids[ch]) {
+        clearInterval(interval_ids[ch]);
+        interval_ids[ch] = void 0;
+    }
+    collector_buffer[ch] = [];
+};
+
+const collectValue = function(channel) {
+    mcp3204.transfer([txData(channel)], function (err, msg) {
+        if (err) throw err;
+
+        if(collector_buffer[channel].length >= COLLECTOR_BUFFER_SIZE) {
+            collector_buffer[channel].pop();
+        }
+        collector_buffer[channel].unshift(extractValue(msg[0]));
+    });
+};
+
+const readyToRead = function() {
+    return collector_buffer[0].length < 30 || collector_buffer[1].length < 30;
+}
+
+const mcp3204 = spi.open(0, 0, function (err) {
+    if (err) throw err;
+    startCollectValue([0, 1]);
+});
+
+const getSamples = function() {
+    // TODO sampleの中にnullやundefinedが入っていた時の対応
+    return [collector_buffer[0].slice(0, 10), collector_buffer[1].slice(0, 10)];
+}
+
+const avg_values = function(samples) {
+    // TODO 配列かチェック
+    if(samples.length == 0) {
+        samples = getSamples();
+    }
+
+    const sum_values = samples.map((values, idx, arr) => {
+        return values.reduce((prev, current, i, arr) => {
+            return prev + current;
+        });
+    });
+
+    return sum_values.map((value, idx, arr) => value/sum_values[idx].length);
+};
+
+const range_values = function(samples) {
+    // TODO 配列かチェック
+    if(samples.length == 0) {
+        samples = getSamples();
+    }
+
+    return samples.map((values, idx, arr) => {
+        let range = Math.max.apply(null, values) - Math.min.apply(null, values);
+        if(!range) {
+            range = 10; // TODO: default value
+        }
+        return range;
+    });
+};
+
+//
 // lock movement controller
 //
 const fs = require('fs');
 const conf_file = './conf.json';
 const STEP_DELAY = 10;
+const MAX_FORWARD_POSITION = 60;
+const FORWARD_INTERVAL = 50; // msec
 
 let conf = {};
 const readConfFile = function() {
@@ -168,51 +221,48 @@ const backwardStepMotor = function(steps) {
     console.log('current pos: ' + getPosition());
 };
 
-const closeLock = function() {
-    if(readyToRead()) {
-        setTimeout(setDefaultPosition, 1000);
-        return;
-    }
-    // TODO sampleの中にnullやundefinedが入っていた時の対応
-    const sample = [collector_buffer[0].slice(0, 10), collector_buffer[1].slice(0, 10)];
-    const value_avg = sample.map((values, idx, arr) => {
-        return values.reduce((prev, current, i, arr) => {
-            return prev + current;
-        });
-    }).map((value, idx, arr) => value/sample[idx].length);
-    let value_range = sample.map((values, idx, arr) => {
-        let range = Math.max.apply(null, values) - Math.min.apply(null, values);
-
-        if(!range) {
-            range = 10; // TODO: default value
-        }
-        return range;
-    });
-
-    // 片側のみ対応
-    const interval = 50; // msec
-    const fd = setInterval(forwardStepMotor, interval, 1, 10);
-    const monitor = function(retry_cnt) {
-        console.log('monitor, current=' + collector_buffer[0][0] + ' count=' + retry_cnt);
-        const threshold = value_avg[0] - (value_range[0] * 5);
-        if(collector_buffer[0][0] <= threshold) {
-            clearTimeout(fd);
-        } else if(retry_cnt < 60) {
-            setTimeout(monitor, interval, ++retry_cnt);
-        } else {
-            console.log('timeout setDefaultPosition');
-            clearTimeout(fd);
-        }
-    };
-    setTimeout(monitor, interval, 0);
-};
-
 const openLock = function() {
     const current_position = getPosition();
     if(current_position <= 0) {
         console.log('openLock: Now lock is open');
     }
     forwardStepMotor(getPosition());
+};
+
+const closeLock = function() {
+    if(readyToRead()) {
+        setTimeout(setDefaultPosition, 1000);
+        return;
+    }
+
+    const samples = getSamples();
+    const avg_values = avg_values(samples);
+    const range_values = range_values(samples);
+    const threshold = avg_values[0] - (range_values[0] * 5);
+
+    const fd = setInterval(forwardStepMotor, FORWARD_INTERVAL, 1, 10);
+
+    const monitor = function(threshold, retry_cnt) {
+        console.log('monitor, current=' + collector_buffer[0][0] + ' count=' + retry_cnt);
+        if(getPosition() > MAX_FORWARD_POSITION) {
+            console.log('setDefaultPosition: max forward position.');
+            clearTimeout(fd);
+            return;
+        }
+
+        if(retry_cnt > 60) {
+            console.log('setDefaultPosition: timeout.');
+            clearTimeout(fd);
+            return;
+        }
+
+        if(collector_buffer[0][0] <= threshold) {
+            clearTimeout(fd);
+        }
+
+        setTimeout(monitor, FORWARD_INTERVAL, ++retry_cnt);
+    };
+    setTimeout(monitor, FORWARD_INTERVAL, threshold, 0);
 };
 
 //

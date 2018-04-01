d34b36b6d65271b9a3484aadabc79e634d4ffe7f
wads <wadslab@gmail.com>
2018-04-01 21:21:54 +0900

    Add toString() function to tpr_105f.js

--- photoreflector/tpr_105f.js
+++ photoreflector/tpr_105f.js
@@ -1,7 +1,12 @@
 'use strict';
 
+/*
+  Photoreflector
+*/
 class Tpr105f {
-    get name() { return 'tpr_105f'; }
+    toString() {
+        return 'tpr_105f';
+    }
 }
 
 module.exports = Tpr105f;
